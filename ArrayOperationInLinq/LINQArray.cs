cd6f160d75df80c3c0c712b866ec98c0ae87487a
Mahesh Nakka <mnakka9@gmail.com>
2017-06-17 19:42:58 +0530

    Intial Commit

---
+++ ArrayOperationInLinq/LINQArray.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class LINQArray
+{
+    public static int GetMaxNoFromArry(Int32[] array)
+    {
+        return array.Max();
+    }
+
+    public static int GetSecondMaxNoFromArry(Int32[] array)
+    {
+        return array.OrderByDescending(i => i).Distinct().Skip(1).First();
+    }
+
+    public static int GetNthMaxNoFromArry(Int32[] array, int nth)
+    {
+        return array.OrderByDescending(i => i).Distinct().Skip(nth).First();
+    }
+
+    public static int GetMinNoFromArry(Int32[] array)
+    {
+        return array.Min();
+    }
+
+    public static int GetSecondMinNoFromArry(Int32[] array)
+    {
+        return array.OrderBy(i => i).Distinct().Skip(1).First();
+    }
+
+    public static int GetNthMinNoFromArry(Int32[] array, int nth)
+    {
+        return array.OrderBy(i => i).Distinct().Skip(nth).First();
+    }
+
+    public static List<Tuple<int,int>> GetPairNumbersWhereSumIsGivenNumber(Int32[] numbersArray, int numbersum, WayofOperation waytodo)
+    {
+        List<Tuple<int, int>> pairs = new List<Tuple<int, int>>();
+        if (waytodo == WayofOperation.Query)
+        {
+            pairs = (from numberone in numbersArray
+                         from numbertwo in numbersArray
+                         where numberone + numbertwo == numbersum
+                         select new Tuple<int, int>(numberone, numbertwo)).Distinct().ToList();
+        }
+        else
+        {
+            pairs = numbersArray.Join(numbersArray,
+                noone => true,
+                notwo => true,
+                (noone, notwo) => new Tuple<int, int>( noone, notwo )
+                ).Where( a => a.Item1 + a.Item2 == 9 ).Distinct().ToList() ;
+
+        }
+        return pairs;
+    }
+
+
+    public static int GetMaxOccurredNumber(Int32[] numbersArray, WayofOperation waytodo)
+    {
+        int maxOccuredNo = 0;
+        if (waytodo == WayofOperation.Query)
+        {
+            maxOccuredNo = (from a in numbersArray
+                            group a by a into g
+                            orderby g.Count() descending
+                            select new { g.Key }
+                            ).First().Key;
+        }
+        else
+        {
+            maxOccuredNo = numbersArray.GroupBy(i => i).OrderByDescending(i => i.Count()).First().Key;
+        }
+
+        return maxOccuredNo;
+    }
+
+
+    public static Tuple<int, int> GetSumOfEvenandOddNumbers(Int32[] numbersArray)
+    {
+        int sumOfEven = 0, sumOfOdd = 0;
+
+        numbersArray.ToList().ForEach(
+
+            i =>
+            {
+
+                if(i % 2 == 0)
+                {
+                    sumOfEven += i;
+                }
+                else
+                {
+                    sumOfOdd += i;
+                }
+
+            }
+
+            );
+
+        return new Tuple<int, int>(sumOfEven,sumOfOdd);
+    }
+
+    public static string CustomFormat(this string s, string arg)
+    {
+         return string.Format("{0} : {1}", arg, s);
+    }
+
+    public static string ToStringForTuple(this Tuple<int,int> s)
+    {
+        return string.Format("      ( {0} , {1} )", s.Item1, s.Item2);
+    }
+
+    public enum WayofOperation
+    {
+        Query,
+        Lambda
+    }
+}
--- ArrayOperationInLinq/Program.cs
+++ ArrayOperationInLinq/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace ArrayOperationInLinq
 {
@@ -6,7 +7,51 @@ namespace ArrayOperationInLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var givenArray = new Int32[] { 1, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 9, 3, 4, 5, 7, 5, 3, 4, 5, 4, 9, 2, 6, 9, 4, 3, 7 };
+
+            // Get the Maximum Number
+            WriteLine(LINQArray.GetMaxNoFromArry(givenArray).ToString().CustomFormat("Max number in Given array is"));
+
+            // Get the second Maximum Number
+            WriteLine(LINQArray.GetSecondMaxNoFromArry(givenArray).ToString().CustomFormat("Second Max number in Given array is"));
+
+            // Get the nth Maximum Number
+            WriteLine(LINQArray.GetNthMaxNoFromArry(givenArray, 5).ToString().CustomFormat("5th Max number in Given array is"));
+
+            // Get the Minimum Number
+            WriteLine(LINQArray.GetMinNoFromArry(givenArray).ToString().CustomFormat("Min number in Given array is"));
+
+            // Get the 2nd Minimum Number
+            WriteLine(LINQArray.GetSecondMinNoFromArry(givenArray).ToString().CustomFormat("Second Min number in Given array is"));
+
+            // Get the nth Minimum Number
+            WriteLine(LINQArray.GetNthMinNoFromArry(givenArray, 5).ToString().CustomFormat("Fifth Min number in Given array is"));
+
+            WriteLine("Possible Combinations of Pair of Numbers using Lamba :");
+
+            foreach (var tuple in LINQArray.GetPairNumbersWhereSumIsGivenNumber(givenArray, 9, LINQArray.WayofOperation.Lambda))
+            {
+                WriteLine(tuple.ToStringForTuple());
+            }
+
+            WriteLine("Possible Combinations of Pair of Numbers using Query :");
+
+            foreach (var tuple in LINQArray.GetPairNumbersWhereSumIsGivenNumber(givenArray, 9, LINQArray.WayofOperation.Query))
+            {
+                WriteLine(tuple.ToStringForTuple());
+            }
+
+            // Get the Max Occured Number
+            WriteLine(LINQArray.GetMaxOccurredNumber(givenArray, LINQArray.WayofOperation.Lambda).ToString().CustomFormat("Maximum occured number in array is"));
+
+            // Get the Sum of even & odd Numbers
+            var sumTuple = LINQArray.GetSumOfEvenandOddNumbers(givenArray);
+
+            WriteLine(sumTuple.Item1.ToString().CustomFormat("Sum of Even numbers in Array"));
+
+            WriteLine(sumTuple.Item2.ToString().CustomFormat("Sum of Odd numbers in Array"));
+
+            Read();
         }
     }
 }
